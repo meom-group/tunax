6f968d4f6cd28118ca0812e0863ccc9b0fcd831c
Gabriel Mouttapa <gabriel.mouttapa@gmail.com>
2024-11-07 14:51:43 +0100

    rtd

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@
 
 import os
 import sys
-sys.path.append(os.path.join('..', 'tunax'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
