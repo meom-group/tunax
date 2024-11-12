494561b606c8432530403d05a1f251b0da667f56
Gabriel Mouttapa <gabriel.mouttapa@gmail.com>
2024-11-12 17:17:28 +0100

    rst

--- docs/conf.py
+++ docs/conf.py
@@ -36,12 +36,12 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # autodoc_mock_imports = ["jax", "xarray", "optax", "netcdf4", "equinox", "jaxtyping"]
 autodoc_default_options = {
-    'undoc-members': False,
+    # 'undoc-members': False,
 #     'private-members': False,
 #     # 'special-members': False,
 #     'inherited-members': False,
 #     'show-inheritance': True,
-#     'member-order': 'bysource'
+    'member-order': 'bysource'
 }
 # -- Napoleon configuration --------------------------------------------------
 napoleon_google_docstring = False
